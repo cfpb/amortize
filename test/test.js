45f559552b09d64e7af689468fc3f97ff34b02d5
Adam Scott <adam.scott@cfpb.gov>
2014-06-10 21:37:54 -0400

    simplify tests

--- test/test.js
+++ test/test.js
@@ -1,21 +1,24 @@
 var amortize = require('../index.js');
 
-exports['After 5 years a borrower with a 30 year, $180,000 loan with a 4.25% interest will have paid $36,583.36 in interest'] = function (test) {
-  test.equal(amortize({amount: 180000, rate: 4.25, totalTerm: 360, amortizeTerm: 60}).interest, 36583.36);
-  test.done();
-};
+var testVal = amortize({amount: 180000, rate: 4.25, totalTerm: 360, amortizeTerm: 60}),
+    testVal2 = amortize({amount: 180000, rate: 4.375, totalTerm: 360, amortizeTerm: 60});
 
-exports['After 5 years a borrower with a 30 year, $180,000 loan with a 4.375% interest will have paid $37,694.10 in interest'] = function (test) {
-  test.equal(amortize({amount: 180000, rate: 4.375, totalTerm: 360, amortizeTerm: 60}).interest, 37694.10);
+exports['After 5 years a borrower with a 30 year, $180,000 loan with a 4.25% interest will have paid $36,583.36 in interest'] = function (test) {
+  test.equal(testVal.interest, 36583.36);
   test.done();
 };
 
 exports['After 5 years a borrower with a 30 year, $180,000 loan with a 4.25% interest will have paid $16,546.15 in principal'] = function (test) {
-  test.equal(amortize({amount: 180000, rate: 4.25, totalTerm: 360, amortizeTerm: 60}).principal, 16546.15);
+  test.equal(testVal.principal, 16546.15);
   test.done();
 };
 
 exports['After 5 years a borrower with a 30 year, $180,000 loan with a 4.25% interest will owe $163,453.85'] = function (test) {
-  test.equal(amortize({amount: 180000, rate: 4.25, totalTerm: 360, amortizeTerm: 60}).balance, 163453.85);
+  test.equal(testVal.balance, 163453.85);
+  test.done();
+};
+
+exports['After 5 years a borrower with a 30 year, $180,000 loan with a 4.375% interest will have paid $37,694.10 in interest'] = function (test) {
+  test.equal(testVal2.interest, 37694.10);
   test.done();
 };
